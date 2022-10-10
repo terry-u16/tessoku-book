756302a663ab416418b22b2ae1eb307050a6bf6d
terry-u16 <terry-u16@users.noreply.github.com>
2022-10-10 11:36:15 +0900

    Initial commit

---
+++ src/bin/c10.rs
@@ -0,0 +1,69 @@
+#[allow(unused_imports)]
+use proconio::*;
+#[allow(unused_imports)]
+use rand::prelude::*;
+
+#[allow(unused_macros)]
+macro_rules! chmin {
+    ($base:expr, $($cmps:expr),+ $(,)*) => {{
+        let cmp_min = min!($($cmps),+);
+        if $base > cmp_min {
+            $base = cmp_min;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! chmax {
+    ($base:expr, $($cmps:expr),+ $(,)*) => {{
+        let cmp_max = max!($($cmps),+);
+        if $base < cmp_max {
+            $base = cmp_max;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! min {
+    ($a:expr $(,)*) => {{
+        $a
+    }};
+    ($a:expr, $b:expr $(,)*) => {{
+        std::cmp::min($a, $b)
+    }};
+    ($a:expr, $($rest:expr),+ $(,)*) => {{
+        std::cmp::min($a, min!($($rest),+))
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! max {
+    ($a:expr $(,)*) => {{
+        $a
+    }};
+    ($a:expr, $b:expr $(,)*) => {{
+        std::cmp::max($a, $b)
+    }};
+    ($a:expr, $($rest:expr),+ $(,)*) => {{
+        std::cmp::max($a, max!($($rest),+))
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! mat {
+    ($e:expr; $d:expr) => { vec![$e; $d] };
+    ($e:expr; $d:expr $(; $ds:expr)+) => { vec![mat![$e $(; $ds)*]; $d] };
+}
+
+#[derive(Debug, Clone)]
+struct Input {}
+
+fn main() {
+    todo!();
+}
